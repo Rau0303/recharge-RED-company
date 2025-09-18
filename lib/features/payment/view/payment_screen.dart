03a8af5bd46dca359a79feb0a8aeb4411a5b0df3
Рауан Айтанатов <rauan@MacBook-Air-Rauan.local>
2025-09-18 15:37:41 +0500

    feat(router): configure AutoRoute with Payment and Success pages

--- lib/features/payment/view/payment_screen.dart
+++ lib/features/payment/view/payment_screen.dart
@@ -1,5 +1,8 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 
+
+@RoutePage()
 class PaymentScreen extends StatefulWidget {
   const PaymentScreen({super.key});
 
